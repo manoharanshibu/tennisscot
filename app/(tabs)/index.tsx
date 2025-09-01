cc6cec02ea585016db027485d8537ae70d518031
shibu <shibum123@gmail.com>
2025-09-01 21:04:57 +0100

    reverted alignment changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -344,10 +344,10 @@ const styles = StyleSheet.create({
     maxHeight: Platform.OS === "web" ? 180 : undefined,
   },
   loginContainer: {
-    // position: 'relative',
+    position: 'relative',
     width: '100%',
     height: '100%',
-    justifyContent: 'center',
+    // justifyContent: 'center',
     alignContent: 'center'
   },
   bannerImage: {
